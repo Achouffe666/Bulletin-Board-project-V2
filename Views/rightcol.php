a699d7ba1a1cb5398f47bd82a1adccfac6123a5d
Roustamkhaydarov <Roustamkhaydarov@gmail.com>
2020-12-01 09:56:51 +0100

    right col.php

---
+++ Views/rightcol.php
@@ -0,0 +1,74 @@
+      <!-- RIGHT COL -->
+
+
+      <div class="col-xl-3 themed-grid-col">
+        
+        <div class="bg-light rounded rounded-pill border mt-5">
+          <div class="input-group">
+            <input type="search" placeholder="Search..."
+              class="form-control  bg-transparent rounded rounded-pill border-0">
+            <div class="input-group-append">
+              <button id="search-glass" type="submit" class="btn btn-link"><i
+                  class="fa fa-search magnifying-glass"></i></button>
+            </div>
+          </div>
+        </div>
+        <!-- END SEARCHBAR -->
+        
+        <hr>
+        
+        <!-- LOGIN -->
+        <div class="login_wrap">
+          
+          <button type="button"
+            class="btn bg-transparent text-black-50 text-left mb-2">
+             Login · Register 
+          </button>
+          
+          <div id="user_login">
+            <div class="input__wrap">
+              <form method="GET" id="submit">
+
+                <div class="form-group">
+                  <label class="text-black-50">
+                    Username
+                  </label>
+                  <input id="username" class="form-control bg-light rounded rounded-pill" type="username">
+                </div>
+
+                <div class="form-group">
+                  <label class="text-black-50">
+                    Password
+                  </label>
+                  <input id="password" class="form-control bg-light rounded rounded-pill" type="password">
+                </div> 
+
+                <div class="form-group">
+                  <div class="checkbox">
+                    <label class="text-black-50"> <input type="checkbox">
+                      Save password 
+                    </label>
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <button type="submit" class=" button--modifier btn text-black border rounded rounded-pill">
+                    Login 
+                  </button>
+                </div> 
+              </form>
+
+              <?php include 'login-php';?>
+
+            </div>
+          </div>
+        </div>
+        <a href="#">
+          Forgot my password
+        </a>
+
+        <!-- END LOGIN -->
+
+        
+      </div>
+      <!--END RIGHT COL-->

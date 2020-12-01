d8ff94de08ca20879ff0628ab78cdf475aa4e047
Achouffe666 <austin@benard.be>
2020-12-01 11:58:36 +0100

    rightcol style

--- views/rightcol.php
+++ views/rightcol.php
@@ -1,15 +1,15 @@
       <!-- RIGHT COL -->
 
 
-      <div class="col-xl-3 themed-grid-col rightcol">
+      <div class="col-xl-3 themed-grid-col rightcol"> 
         
-        <div class="bg-light rounded rounded-pill border mt-5">
+        <div class="bg-light rounded rounded-pill border mt-5"> 
           <div class="input-group">
             <input type="search" placeholder="Search..."
-              class="form-control  bg-transparent rounded rounded-pill border-0">
+              class="form-control  bg-transparent rounded rounded-pill border-0"> 
             <div class="input-group-append">
-              <button id="search-glass" type="submit" class="btn btn-link"><i
-                  class="fa fa-search magnifying-glass"></i></button>
+              <button id="search-glass" type="submit" class="btn btn-link"><i 
+                  class="fa fa-search magnifying-glass"></i></button> 
             </div>
           </div>
         </div>
@@ -51,7 +51,7 @@
                   </div>
                 </div>
 
-                <div class="form-group">
+                <div class="form-group d-flex justify-content-center">
                   <button type="submit" class=" button--modifier btn text-black border rounded rounded-pill">
                     Login 
                   </button>
@@ -68,7 +68,44 @@
         </a>
 
         <!-- END LOGIN -->
+        
+        <!-- last posts  -->
+        <div class="card topics-top">
+          <div class="card-header topics-top-title">
+            <h4>Last posts</h4>
+          </div>
+            <div class="card-body last-topics-wrap">
+              <div> 
+                <span class="content-title">titre (php)</span> <span class="float-right">2 hours ago</span>
+                  <div>
+                    <span>contents contents</span>
+              </div>
+                  </div>
+              </div>
+        </div>
+    <!-- end last posts  -->
 
+    <!-- last activer users -->
+    <div class="card user-top">
+          <div class="card-header user-top-title">
+            <h4>Last active users</h4>
+          </div>
+            <div class="row d-flex flex-row align-items-start card-body last-user-wrap">
+              <div class="col-4"> 
+              <img src="../static/image/3.png" alt="User1" style="width:75px">
+              <p>#User1</p>             
+             </div>
+              <div class="col-4"> 
+              <img src="../static/image/3.png " alt="user2" style="width:75px">
+              <p>#User2</p>             
+              </div>
+              <div class="col-4"> 
+              <img src="../static/image/3.png" alt="user3" style="width:75px">
+              <p>#User</p>             
+              </div>
+                  </div>
+              </div>
+        </div>
         
       </div>
       <!--END RIGHT COL-->

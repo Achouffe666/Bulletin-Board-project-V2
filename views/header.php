14b2fb9db43fa5f6931cb5b3cf381b0af21937e1
Roustamkhaydarov <Roustamkhaydarov@gmail.com>
2020-12-01 17:04:54 +0100

    topics pae

--- views/header.php
+++ views/header.php
@@ -13,7 +13,8 @@
     <link rel="stylesheet" href="../static/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet">
 </head>
 <body>
      

6dfc9c17f05944202a2beac15d7a2dc8218897ec
Maud-Pixel <leleuxmaud@gmail.com>
2020-12-01 09:48:31 +0100

    footer responsive

--- Views/footer.php
+++ Views/footer.php
@@ -1,22 +1,22 @@
 <footer id="footer">
     
-    <div class="row-fluid row-social-media d-flex justify-content-center align-items-center">
-        <div class="col-1 social-media__circle  ">
+    <div class="row row-fluid row-social-media d-flex justify-content-between justify-content-md-center align-items-center">
+        <div class="col-4 col-md-1 social-media__circle  ">
             <a class="social-media__link d-flex justify-content-center align-items-center" href="https://twitter.com/?lang=fr"><i class="social-media__icon fa fa-twitter"></i></a>
         </div>
-        <div class="col-1 social-media__circle">
+        <div class="col-4 col-md-1 social-media__circle">
             <a class="social-media__link d-flex justify-content-center align-items-center" href="https://www.apple.com/"><i class="social-media__icon fa fa-apple"></i></a>
         </div>
-        <div class="col-1 social-media__circle ">
+        <div class="col-4 col-md-1 social-media__circle ">
             <a class="social-media__link d-flex justify-content-center align-items-center" href="https://fr-fr.facebook.com"><i class="social-media__icon fa fa-facebook"></i></a>
         </div>
-        <div class="col-1 social-media__circle">
+        <div class="col-4 col-md-1 social-media__circle">
             <a class="social-media__link d-flex justify-content-center align-items-center" href="https://codepen.io"><i class="social-media__icon fa fa-codepen"></i></a>
         </div>
-        <div class="col-1 social-media__circle">
+        <div class="col-4 col-md-1 social-media__circle">
             <a class="social-media__link d-flex justify-content-center align-items-center" href="https://digg.com/"><i class="social-media__icon fa fa-digg"></i></a>
         </div>
-        <div class="col-1 social-media__circle">
+        <div class="col-4 col-md-1 social-media__circle">
             <a class="social-media__link d-flex justify-content-center align-items-center" href="https://www.pinterest.fr/">
                 <i class="social-media__icon fa fa-pinterest-p"></i>
             </a> 
@@ -24,10 +24,18 @@
 
     </div>
 
-    <div class="row-fluid row_details">
+    <div class="row row-fluid row_more-infos d-flex justify-content-between justify-content-md-end">
+       
+            <a class="col-4 col-md-1 more-infos__link" href="#"> <i class="fas fa-envelope"></i>Contact us </a>
+            <a class="col-4 col-md-1 more-infos__link" href="#"> <i class="fas fa-shield-alt"></i> The team </a>
+            <a class="col-4 col-md-1 more-infos__link" href="./terms.html"> <i class="fas fa-check"></i> Terms </a>
+            <a class="col-4 col-md-1 more-infos__link" href="./policy.html"> <i class="fas fa-lock"></i> Privacy </a>
+            <a class="col-4 col-md-1 more-infos__link" href="./member.php"> <i class="fas fa-users"></i> Members </a>
+            <a class="col-4 col-md-2 more-infos__link p0 " href="./logout.php"> <i class="fas fa-trash-alt"></i> Delete cookies </a>
+       
     </div>
 
-    <div class="row-fluid row_end">
+    <div class="row row-fluid row_end">
     </div>
      
 

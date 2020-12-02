fac47766ffdce152ff87a9c27bc29f07aab51e05
Roustamkhaydarov <Roustamkhaydarov@gmail.com>
2020-12-02 19:48:17 +0100

    removed float-right

--- views/rightcol.php
+++ views/rightcol.php
@@ -1,6 +1,6 @@
       <!-- RIGHT COL -->
    
-      <div class="col-xl-3 themed-grid-col rightcol float-right"> 
+      <div class="col-xl-3 themed-grid-col rightcol"> 
         
         <div class="bg-light rounded rounded-pill border mt-5"> 
           <div class="input-group">

71cd3553ea584e391baa84b8025dea05fc97591c
Achouffe666 <austin@benard.be>
2020-12-02 12:03:21 +0100

    path in index

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 ?>
-<?php include 'header.php';?>
+<?php include 'views/header.php';?>
 
-<?php include "rightcol.php";?>
-<?php include 'footer.php';?>
+<?php include "views/rightcol.php";?>
+<?php include 'views/footer.php';?>
 
 
 

915e991cbeb3cec6aa160e4f96c40b2f0e0aa935
Roustamkhaydarov <Roustamkhaydarov@gmail.com>
2020-12-02 19:49:50 +0100

    removed fixed-bottom on footer

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,4 @@
-<footer id="footer" class="fixed-bottom">
+<footer id="footer" class="bottom">
     
     <div class="row social-bar row-fluid row-social-media d-flex justify-content-between justify-content-md-center align-items-center">
         <div class="col-4 col-md-1 social-media__circle  ">

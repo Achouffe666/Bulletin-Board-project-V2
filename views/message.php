d8b43a9a41a7ba5792ca5d1d3a6533ae4ed9b59c
Roustamkhaydarov <Roustamkhaydarov@gmail.com>
2020-12-02 16:43:29 +0100

    message front

--- views/message.php
+++ views/message.php
@@ -7,40 +7,31 @@
    
 ?>
 
-<div class="container-fluid overlay">
-        <div class="breadcrumb">
-            <nav aria-label="breadcrumb container-fluid">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="inscription.php">Home</a></li>
-                    <li class="breadcrumb-item"><a href="topic.php">Topic</a></li>
-                    <li class="breadcrumb-item"><a href="message.php">Message</a></li>
-                </ol>
-            </nav>
-        </div>
-        <?php include "../database/database.php";
-            include "../controlers/functions_message.php";
-            global $db;
-        ?>
+<div class="container-fluid overlay position-relative rounded-lg main__wrap d-flex flex-column">
+    
+    <nav class="nav-list">
+        <ol class="breadcrumb bg-transparent pt-5">
+            <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i> Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Board Index</a></li>
+            <li class="breadcrumb-item"><a href="#">Category One</a></li>
+            <li class="breadcrumb-item">Forum One</li>
+        </ol>
+    </nav>
 
-        <div class="row">
-            <div class="col col-md-2">
-                <button id="button_reply" type="submit" class="btn  btn-outline-info  button-reply" name="post_reply">Post reply</button>
-            </div>
-            <div class="col col-6-md search">
-                <form action="message_search.php" method="post">
-                    <div class="form-group" >
-                        <div class="input-group-prepend">
-                            <input type="text" class="form-control" id="search" value="" name="search">
-                            <button type="submit" class="btn btn-update mb-2"><img src="../images/search.svg" alt="search"></button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-       
-        
-        <div class="row content row-content justify-content-center ">
-                <div class="row row-message row-message2">
+    <?php include "../database/database.php";
+        include "../controlers/functions_message.php";
+        global $db;
+    ?>
+
+    <div class="board__inner row">
+
+        <div class="board__wrap col-xl-9 mr-0 mb-2">
+
+            <!-- MESSAGES WRAP -->
+            <div class="container row-content justify-content-center ">
+
+                <!-- MESSAGE CREATE -->
+                <div class="row row-message row-message2 mb-5 p-2 bg-light">
                     <div class="col-2 col-content-message">
                         <img class="card-img-top img-fluid message-photo d-block mx-auto" src="../images/avatar_autre.jpg" style="width: 150px;" alt="avatar_autre">
                         <p name="message-position"><?php get_user_position()?></p>
@@ -57,9 +48,11 @@
                         </form>
                     </div>
                 </div>
+                <!-- END MESSAGE CREATE -->
             
-           
-               <div class="row row-message">
+                <!-- MESSAGE START -->
+                <div class="row row-message mb-2 bg-light pt-2 pr-2">
+
                     <div class="col-2 col-content-message">
                        
                         <img class="card-img-top img-fluid message-photo d-block mx-auto" src=<?php 
@@ -67,23 +60,43 @@
                         <p class="message-position"><?php get_message_position();?></p>
                         <p class="message-identity"><?php get_message_nickname(); ?></p>
                         <p class="message-number"><?php get_message_count()?> post(s)</p>
+
                     </div>
+
                     <div class="col-10 col-content-message content-message2">
-                        <p><?php get_message_title()?></p>
-                        <p><?php get_message_content()?></p>
-                        <p class="message-signature"><?php get_message_signature();?></p>
-                        <p><?php get_message_creation_date();?></p>
-                       
+
+                        <div class="row'">
+                            <p class="col-3"><?php get_message_title()?></p>
+                            <p class="col-6"><?php get_message_content()?></p>
+                            <p class="message-signature col-2"><?php get_message_signature();?></p>
+                            <p class="col-1"><?php get_message_creation_date();?></p>
+                        </div>
+
+                        <div class="row"> 
+                            <p class="message__content">
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+
+                            </p>
+                        </div>
+
                         <button id="delete" type="submit" name="message_deleted"  class="btn btn-outline-warning mb-2">
                             <a href="message_delete.php?id=<?php get_message_id()?>">Annuler</a> 
                         </button>
                         
                     </div>
-                </div>
-               
 
-             </div>  
-            
+                </div>
+                <!-- END MESSAGE EXEMPLE -->
+            </div>
+            <!-- END MESSAGE WRAP -->
+
+        </div>
+        <!-- END BOARD__WRAP -->
+
+        <?php include "rightcol.php";?>
     </div>
+    <!-- END BOARD__INNER -->  
+</div>
+<!-- END MAIN WRAP -->
     
   <?php include "footer.php"; ?>

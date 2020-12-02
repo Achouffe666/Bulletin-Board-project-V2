53139240e790269ee2d5ea4e67a22ec9be84bda8
Roustamkhaydarov <Roustamkhaydarov@gmail.com>
2020-12-02 09:44:22 +0100

    board front

---
+++ views/boards.php
@@ -0,0 +1,409 @@
+<?php include "header.php";?>
+
+<!-- BOARD -->
+<div class="container overlay main__wrap d-flex flex-column rounded-lg my-3 pb-3 pr-2 bg-white shadow">
+    <nav class="nav__list">
+        <ol class="breadcrumb bg-transparent pt-5">
+            <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i> Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Board Index</a></li>
+            <li class="breadcrumb-item"><a href="#">Category One</a></li>
+            <li class="breadcrumb-item">Forum One</li>
+        </ol>
+    
+    </nav>
+    <div class="board__inner row container-fluid">
+        
+        <div class="board__wrap col-xl-9 mr-0 mb-2">
+        
+            <!-- BOARD ONE TOPICS-->
+            <h4 class="my-5 ml-5 text-black-50">Category One</h4>
+            <!-- TOPICS WRAP-->
+            <div class="topics__wrap container-fluid d-flex p-1 mb-5">
+                <!-- ONE LINK TO TOPICS-->
+                <div class="topics bg-white shadow-sm rounded p-1 m-1">
+        
+                    <div class="topics__container d-flex flex-column p-2">
+        
+                        <div class="row no-gutters topics__top">
+        
+                            <div class="col-3 mr-0">
+                                <img src="1.png">
+                            </div>
+        
+                            <div class="col-9">
+                                <h4 class="topics__title"><a class ="" href="">Topics_one  </a><i class="fas fa-check"></i></h4>
+                                <p class="topics__description">This grouping is really cool</p>
+                            </div>
+        
+                        </div>
+                        <hr class="topics__hr float-left m-0 mb-3">
+                        <div class="row topics__bot no-gutters">
+        
+                            <div class="col-3 d-flex flex-column align-items-center">
+                                <p class="topics__number">6</p>
+                                <p class="topics__text">Topics</p>
+                            </div>
+        
+                            <div class="col-3 d-flex flex-column align-items-center">
+                                <p class="topics__number">33</p>
+                                <p class="topics__text">Posts</p>
+                            </div>
+        
+                            <div class="col-6 d-flex flex-column align-items-center">
+                                <p class="topics__date">Sun 8 Nov</p>
+                                <p class="topics__text">Last post</p>
+                            </div>
+        
+                        </div>
+        
+                    </div>
+        
+                </div>
+                <!-- END ONE LINK TO TOPICS-->
+        
+        
+        
+                <!-- ONE LINK TO TOPICS-->
+                <div class="topics bg-white shadow-sm rounded p-1 m-1">
+        
+                    <div class="topics__container d-flex flex-column pl-2">
+        
+                        <div class="row topics__top">
+        
+                            <div class="col-3">
+                                <img src="2.png">
+                            </div>
+        
+                            <div class="col-9">
+                                <h4 class="topics__title"><a href="">Topics two </a><i class="fas fa-check"></i></h4>
+                                <p class="topics__description">This grouping is really cool</p>
+                            </div>
+        
+                        </div>
+                        <hr class="topics__hr float-left m-0 mb-3">
+                        <div class="row topics__bot">
+        
+                            <div class="col-3 d-flex flex-column align-items-center">
+                                <p class="topics__number">6</p>
+                                <p class="topics__text">Topics</p>
+                            </div>
+        
+                            <div class="col-3 d-flex flex-column align-items-center">
+                                <p class="topics__number">33</p>
+                                <p class="topics__text">Posts</p>
+                            </div>
+        
+                            <div class="col-6 d-flex flex-column align-items-center">
+                                <p class="topics__date">Sun 8 Nov</p>
+                                <p class="topics__text">Last post</p>
+                            </div>
+        
+                        </div>
+        
+                    </div>
+        
+                </div>
+                <!-- END ONE LINK TO TOPICS-->
+        
+        
+        
+        
+        
+        
+            </div>
+            <!-- END CATEGORY ONE TOPICS-->
+        
+        <!-- BOARD TWO TOPICS-->
+        <h4 class="my-5 ml-5 text-black-50">Rock</h4>
+        <!-- TOPICS WRAP-->
+        <div class="topics__wrap container-fluid d-flex flex-wrap mb-5">
+            <!-- ONE LINK TO TOPICS-->
+            <div class="topics bg-white shadow-sm rounded p-1 m-1">
+        
+                <div class="topics__container d-flex flex-column pl-2">
+        
+                    <div class="row topics__top">
+        
+                        <div class="col-3">
+                            <img src="3.png">
+                        </div>
+        
+                        <div class="col-9">
+                            <h4 class="topics__title"><a href="">Pink Floyd </a><i class="fas fa-check"></i></h4>
+                            <p class="topics__description">This grouping is really cool</p>
+                        </div>
+        
+                    </div>
+                    <hr class="topics__hr float-left m-0 mb-3">
+                    <div class="row topics__bot">
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">6</p>
+                            <p class="topics__text">Topics</p>
+                        </div>
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">33</p>
+                            <p class="topics__text">Posts</p>
+                        </div>
+        
+                        <div class="col-6 d-flex flex-column align-items-center">
+                            <p class="topics__date">Sun 8 Nov</p>
+                            <p class="topics__text">Last post</p>
+                        </div>
+        
+                    </div>
+        
+                </div>
+        
+            </div>
+            <!-- END ONE LINK TO TOPICS-->
+        
+        
+        
+            <!-- ONE LINK TO TOPICS-->
+            <div class="topics bg-white shadow-sm rounded p-1 m-1">
+        
+                <div class="topics__container d-flex flex-column pl-2">
+        
+                    <div class="row topics__top">
+        
+                        <div class="col-3">
+                            <img src="4.png">
+                        </div>
+        
+                        <div class="col-9">
+                            <h4 class="topics__title"><a href="#">Slayer </a><i class="fas fa-check"></i></h4>
+                            <p class="topics__description">This grouping is really cool </p>
+                        </div>
+        
+                    </div>
+                    <hr class="topics__hr float-left m-0 mb-3">
+                    <div class="row topics__bot">
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">6</p>
+                            <p class="topics__text">Topics</p>
+                        </div>
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">33</p>
+                            <p class="topics__text">Posts</p>
+                        </div>
+        
+                        <div class="col-6 d-flex flex-column align-items-center">
+                            <p class="topics__date">Sun 8 Nov</p>
+                            <p class="topics__text">Last post</p>
+                        </div>
+        
+                    </div>
+        
+                </div>
+        
+            </div>
+            <!-- END ONE LINK TO TOPICS-->
+        
+        
+        
+        
+        
+        
+        </div>
+        <!-- END BOARD TWO TOPICS-->
+        
+        
+        
+        <!-- BOARD THREE TOPICS-->
+        <h4 class="my-5 ml-5 text-black-50">Rap</h4>
+        <!-- TOPICS WRAP-->
+        <div class="topics__wrap container-fluid d-flex flex-wrap mb-5">
+            <!-- ONE LINK TO TOPICS-->
+            <div class="topics bg-white shadow-sm rounded p-1 m-1">
+        
+                <div class="topics__container d-flex flex-column pl-2">
+        
+                    <div class="row topics__top">
+        
+                        <div class="col-3">
+                            <img src="1.png">
+                        </div>
+        
+                        <div class="col-9">
+                            <h4 class="topics__title"><a href="#">Snoop Dogg </a><i class="fas fa-check"></i></h4>
+                            <p class="topics__description">This grouping is really cool</p>
+                        </div>
+        
+                    </div>
+                    <hr class="topics__hr float-left m-0 mb-3">
+                    <div class="row topics__bot">
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">6</p>
+                            <p class="topics__text">Topics</p>
+                        </div>
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">33</p>
+                            <p class="topics__text">Posts</p>
+                        </div>
+        
+                        <div class="col-6 d-flex flex-column align-items-center">
+                            <p class="topics__date">Sun 8 Nov</p>
+                            <p class="topics__text">Last post</p>
+                        </div>
+        
+                    </div>
+        
+                </div>
+        
+            </div>
+            <!-- END ONE LINK TO TOPICS-->
+        
+            <!-- ONE LINK TO TOPICS-->
+            <div class="topics bg-white shadow-sm rounded p-1 m-1">
+        
+                <div class="topics__container d-flex flex-column pl-2">
+        
+                    <div class="row topics__top">
+        
+                        <div class="col-3">
+                            <img src="5.png">
+                        </div>
+        
+                        <div class="col-9">
+                            <h4 class="topics__title"><a href="#">Maud </a><i class="fas fa-check"></i></h4>
+                            <p class="topics__description">Tu peux pas test mon skill lorem ipsum </p>
+                        </div>
+        
+                    </div>
+                    <hr class="topics__hr float-left m-0 mb-3">
+                    <div class="row topics__bot">
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">6</p>
+                            <p class="topics__text">Topics</p>
+                        </div>
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">33</p>
+                            <p class="topics__text">Posts</p>
+                        </div>
+        
+                        <div class="col-6 d-flex flex-column align-items-center">
+                            <p class="topics__date">Sun 8 Nov</p>
+                            <p class="topics__text">Last post</p>
+                        </div>
+        
+                    </div>
+        
+                </div>
+        
+            </div>
+            <!-- END ONE LINK TO TOPICS-->
+        
+        
+        
+            <!-- ONE LINK TO TOPICS-->
+            <div class="topics bg-white shadow-sm rounded p-1 m-1">
+        
+                <div class="topics__container d-flex flex-column pl-2">
+        
+                    <div class="row topics__top">
+        
+                        <div class="col-3">
+                            <img src="1.png">
+                        </div>
+        
+                        <div class="col-9">
+                            <h4 class="topics__title"><a href="#">Eminem </a><i class="fas fa-check"></i></h4>
+                            <p class="topics__description">This grouping is really cool</p>
+                        </div>
+        
+                    </div>
+                    <hr class="topics__hr float-left m-0 mb-3">
+                    <div class="row topics__bot">
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">6</p>
+                            <p class="topics__text">Topics</p>
+                        </div>
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">33</p>
+                            <p class="topics__text">Posts</p>
+                        </div>
+        
+                        <div class="col-6 d-flex flex-column align-items-center">
+                            <p class="topics__date">Sun 8 Nov</p>
+                            <p class="topics__text">Last post</p>
+                        </div>
+        
+                    </div>
+        
+                </div>
+        
+            </div>
+            <!-- END ONE LINK TO TOPICS-->
+        
+        
+            <!-- ONE LINK TO TOPICS-->
+            <div class="topics bg-white shadow-sm rounded p-1 m-1">
+        
+                <div class="topics__container d-flex flex-column pl-2">
+        
+                    <div class="row topics__top">
+        
+                        <div class="col-3">
+                            <img src="2.png">
+                        </div>
+        
+                        <div class="col-9">
+                            <h4 class="topics__title"><a href="#">Tupac </a><i class="fas fa-check"></i></h4>
+                            <p class="topics__description">This grouping is really cool </p>
+                        </div>
+        
+                    </div>
+                    <hr class="topics__hr float-left m-0 mb-3">
+                    <div class="row topics__bot">
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">6</p>
+                            <p class="topics__text">Topics</p>
+                        </div>
+        
+                        <div class="col-3 d-flex flex-column align-items-center">
+                            <p class="topics__number">33</p>
+                            <p class="topics__text">Posts</p>
+                        </div>
+        
+                        <div class="col-6 d-flex flex-column align-items-center">
+                            <p class="topics__date">Sun 8 Nov</p>
+                            <p class="topics__text">Last post</p>
+                        </div>
+        
+                    </div>
+        
+                </div>
+        
+            </div>
+            <!-- END ONE LINK TO TOPICS-->
+        
+        
+        
+        
+        
+        
+        </div>
+        <!-- END BOARD THREE TOPICS-->
+        
+        
+        
+        
+        </div>
+        <!-- END OF BOARD WRAP-->
+        <?php include "rightcol.php";?>
+    </div>
+    <!-- END OF BOARD INNER-->
+</div>
+<!-- END OF CONTAINER-->
+<?php include "footer.php";?>

6d0a50b81caf8e147e1d2b235cea461e1d6cdf31
Roustamkhaydarov <Roustamkhaydarov@gmail.com>
2020-12-03 10:59:00 +0100

    added post reply to message + updated scss

--- views/message.php
+++ views/message.php
@@ -21,9 +21,52 @@
         global $db;
     ?>
 
+    <div class="row mb-2">
+        <div class="col col-md-2">
+            <button id="button_reply" type="submit" class="button--modifier px-3 py-1  btn-outline-info  button-reply" name="post_reply">Post reply</button>
+        </div>
+            
+        <div class="col col-6-md search">
+            <form action="message_search.php" method="post">
+                <div class="form-group" >
+                    <div class="input-group-prepend">
+                        <input type="text" class="form-control" id="search" value="" name="search">
+                        <button type="submit" class="button--modifier btn-update"><img src="../images/search.svg" alt="search"></button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submit">
+        </form>
+    </div>
+
     <div class="board__inner row">
 
         <div class="board__wrap col-xl-9 mr-0 mb-2">
+            <!-- MESSAGE CREATE -->
+            <div class="row row-message row-message2 mb-5 p-2 bg-light">
+                <div class="col-2 col-content-message">
+                    <img class="card-img-top img-fluid message-photo d-block mx-auto" src="../images/avatar_autre.jpg" style="width: 150px;" alt="avatar_autre">
+                    <p name="message-position"><?php get_user_position()?></p>
+                    <p name="message-identity"><?php get_user_nickname()?></p>
+                </div>
+
+                <div class="col-10 col-content-message row-message2">
+                    <form method="post" action="message_post.php">
+                        <p>Titre :</p>
+                        <input type="text" class="form-control" name="message_name">
+                        <p>Write your message</p>
+                        <textarea class="form-control" name="content"></textarea>
+                        <button id="record" type="submit" class="btn btn-outline-info mb-2">Sauvegarder</button>
+                        <button id="cancel" type="submit" class="btn btn-outline-warning mb-2">Annuler</button>
+                    </form>
+                </div>
+            </div>
+            <!-- END MESSAGE CREATE -->
 
             <!-- MESSAGES WRAP -->
             <div class="container row-content justify-content-center ">
@@ -32,25 +75,7 @@ $infos_message = get_message();
 foreach($infos_message as $infos)
 {
 ?>
-                <!-- MESSAGE CREATE -->
-                <div class="row row-message row-message2 mb-5 p-2 bg-light">
-                    <div class="col-2 col-content-message">
-                        <img class="card-img-top img-fluid message-photo d-block mx-auto" src="../images/avatar_autre.jpg" style="width: 150px;" alt="avatar_autre">
-                        <p name="message-position"><?php get_user_position()?></p>
-                        <p name="message-identity"><?php get_user_nickname()?></p>
-                    </div>
-                    <div class="col-10 col-content-message row-message2">
-                        <form method="post" action="message_post.php">
-                            <p>Titre :</p>
-                            <input type="text" class="form-control" name="message_name">
-                            <p>Write your message</p>
-                            <textarea class="form-control" name="content"></textarea>
-                            <button id="record" type="submit" class="btn btn-outline-info mb-2">Sauvegarder</button>
-                            <button id="cancel" type="submit" class="btn btn-outline-warning mb-2">Annuler</button>
-                        </form>
-                    </div>
-                </div>
-                <!-- END MESSAGE CREATE -->
+
 <?php } ?>
                 <!-- MESSAGE START -->
                 <div class="row row-message mb-2 bg-light pt-2 pr-2">

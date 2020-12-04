e7c0a43407f572bb7948156c7488e8b1511ba4be
Maud-Pixel <leleuxmaud@gmail.com>
2020-12-04 09:51:22 +0100

    message mise à jour hier

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -2,7 +2,7 @@
     document.querySelector('#button_reply').addEventListener('click',()=>{
       
         document.querySelector('.row-message2').classList.add('row-message3');
-        document.querySelector('.row-message2').classList.remove('row-message2')
+        document.querySelector('.row-message2').classList.remove('row-message2');
     });
     document.querySelector('#cancel').addEventListener('click',()=>{
         document.querySelector('.row-message3').classList.add('row-message2');

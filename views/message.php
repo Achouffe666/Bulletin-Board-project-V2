4c0e085a6e6ae62da88b3f37f810a6a9728494cd
Roustamkhaydarov <Roustamkhaydarov@gmail.com>
2020-12-04 11:04:26 +0100

    fixing image b-rad + height

--- views/message.php
+++ views/message.php
@@ -23,7 +23,7 @@
 
     <div class="row mb-2 ml-2">
         <div class="col col-md-2">
-            <button id="button_reply" type="submit" class="button--modifier px-3 py-1  rounded rounded-pill  border-0  button-reply" name="post_reply">Post reply</button>
+            <button id="button_reply" type="submit" class="button--modifier px-3 py-1  rounded rounded-pill  border-0  button-reply" name="post_reply">Post reply <i class="fas fa-pencil-alt"></i></button>
         </div>
             
           <div class="bg-light rounded rounded-pill border ml-3">

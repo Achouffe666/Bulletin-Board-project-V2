5a77764d0a1902c802b026d3a11c6768e727e53a
Roustamkhaydarov <Roustamkhaydarov@gmail.com>
2020-12-04 15:56:10 +0100

    changed profil a bit

--- views/profil.php
+++ views/profil.php
@@ -20,17 +20,17 @@
     ?>
         <div class="row content row-content justify-content-center ">
             <div class="card">
-                <div class="card-header d-flex justify-content-center">
+                <div class="card-header profil__image d-flex justify-content-center">
                     <img class="card-img-top img-fluid card-profil " src=<?php echo $avatar ?> alt="avatar" style="width: 150px;">
                 </div>
                 <!-- UPLOAD IMAGE -->
-                <form action="" method="post" enctype="multipart/form-data">
+                <form action="" method="post" class="mx-1" enctype="multipart/form-data">
                     <input type="file" name="fileToUpload" id="fileToUpload">
                     <input type="submit" value="Upload Image" name="submit" >
                 </form>
                 <!-- UPLOAD IMAGE -->
                 <div class="card-body">
-                    <p class="h1">Profile</p>
+                    <p class="h1 d-flex justify-content center text-black-50">Profile</p>
                     <form method="post" action="inscription_update.php">
                         <div class="form-group" >
                             <label for="nickname">Pseudo</label>

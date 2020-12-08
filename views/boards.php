1ebaf01e5fa68f1d02365eab047e61f14461cf47
Roustamkhaydarov <Roustamkhaydarov@gmail.com>
2020-12-08 14:00:41 +0100

    update topics ml-1 fa check

--- views/boards.php
+++ views/boards.php
@@ -35,7 +35,7 @@
                                 <img src="../static/image/<?=$board['id']%5?>.png">
                             </div>
                             <div class="col-9">
-                                <h4 class="topics__title"><a class ="" href="views/topics.php?boardId=<?=$board['id']?>"><?=$board['subject']?></a><i class="fas fa-check"></i></h4>
+                                <h4 class="topics__title"><a class ="" href="views/topics.php?boardId=<?=$board['id']?>"><?=$board['subject']?></a><i class="fas fa-check ml-1"></i></h4>
                                 <p class="topics__description"><?=$board['description']?></p>
                             </div>
         

9c7f5dff6a10d09fedd2029afb29c7445ff1882d
Roustamkhaydarov <Roustamkhaydarov@gmail.com>
2020-12-08 16:08:12 +0100

    removed board secret

--- controlers/functions_boards.php
+++ controlers/functions_boards.php
@@ -15,23 +15,6 @@ function get_boards()
     return $result;
 
 }
-function board_secret()
-{
-    global $db;
-    global $board_id;
-
-    $db->query("SELECT * FROM boards WHERE board_id = $board_id");
-
-
-    if($board_id==13){
-        $result = echo "#";
-    }
-    else{
-        $result = echo "views/topics.php?boardId=<?=$board['id']?>";
-    }
-    
-    return $result;
-}
 
 ?>
 

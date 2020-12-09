b73392636f95e94492c0ad8fe72fc66f0eef393e
Achouffe666 <austin@benard.be>
2020-12-09 11:45:20 +0100

    adding userid on topic creation

--- controlers/functions_topics.php
+++ controlers/functions_topics.php
@@ -61,10 +61,11 @@ function create_topic(){
         $count = $query->rowCount();
 
        if ($count == 0){
-    $query = $db->prepare('INSERT INTO topics(title, board_id) VALUES (:title,:board_id);');
+    $query = $db->prepare('INSERT INTO topics(title, board_id, user_id) VALUES (:title,:board_id, :user_id);');
     $query->execute([
         'title' => $_POST['Title'],
         'board_id' => $boardid,
+        'user_id' => $_SESSION['id'],
         ]);
 ?>     
      <div class="alert alert-success" role="alert">
